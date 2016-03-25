a379ae6bb077bd3aff3810ae2fd3ee190499f706
Medicean <Medici.Yan@Gmail.com>
2016-03-26 03:08:46 +0800

    添加 jsp shell 样本,修正custom模式数据库部分逻辑

--- source/core/custom/template/database/default.jsx
+++ source/core/custom/template/database/default.jsx
@@ -1,11 +1,12 @@
-// 
+//
 // 默认代码模板
-// 
+//
 // @params
 // :encode  SHELL编码
 // :conn    数据库连接字符串
 // :sql     执行SQL语句
-// 
+// :db      数据库名
+// :table   表名
 
 module.exports = {
   show_databases: {
@@ -16,12 +17,15 @@ module.exports = {
   show_tables: {
     _: 'O',
     'z0': '#{encode}',
-    'z1': '#{conn}'
+    'z1': '#{conn}',
+    'z2': '#{db}'
   },
   show_columns: {
     _: 'P',
     'z0': '#{encode}',
-    'z1': '#{conn}'
+    'z1': '#{conn}',
+    'z2': '#{db}',
+    'z3': '#{table}'
   },
   query: {
     _: 'Q',
