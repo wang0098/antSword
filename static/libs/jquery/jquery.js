efa92fd9ac9a071876442cfee7d25143592e9ada
antoor <u@uyu.us>
2016-06-01 15:20:43 +0800

    Adjust and optimize the code
    调整优化代码

--- static/libs/jquery/jquery.js
+++ static/libs/jquery/jquery.js
@@ -14,25 +14,27 @@
 
 (function( global, factory ) {
 
-	if ( typeof module === "object" && typeof module.exports === "object" ) {
-		// For CommonJS and CommonJS-like environments where a proper `window`
-		// is present, execute the factory and get jQuery.
-		// For environments that do not have a `window` with a `document`
-		// (such as Node.js), expose a factory as module.exports.
-		// This accentuates the need for the creation of a real `window`.
-		// e.g. var jQuery = require("jquery")(window);
-		// See ticket #14549 for more info.
-		module.exports = global.document ?
-			factory( global, true ) :
-			function( w ) {
-				if ( !w.document ) {
-					throw new Error( "jQuery requires a window with a document" );
-				}
-				return factory( w );
-			};
-	} else {
-		factory( global );
-	}
+	// 不使用module加载器，这样就可以直接在electron中引用jquery了
+	factory(global);
+	// if ( typeof module === "object" && typeof module.exports === "object" ) {
+	// 	// For CommonJS and CommonJS-like environments where a proper `window`
+	// 	// is present, execute the factory and get jQuery.
+	// 	// For environments that do not have a `window` with a `document`
+	// 	// (such as Node.js), expose a factory as module.exports.
+	// 	// This accentuates the need for the creation of a real `window`.
+	// 	// e.g. var jQuery = require("jquery")(window);
+	// 	// See ticket #14549 for more info.
+	// 	module.exports = global.document ?
+	// 		factory( global, true ) :
+	// 		function( w ) {
+	// 			if ( !w.document ) {
+	// 				throw new Error( "jQuery requires a window with a document" );
+	// 			}
+	// 			return factory( w );
+	// 		};
+	// } else {
+	// 	factory( global );
+	// }
 
 // Pass this if window is not defined yet
 }(typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
