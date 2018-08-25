032257eac222e6537523063d3c8a86cf1f5996cb
Medicean <Medici.Yan@Gmail.com>
2018-08-25 19:11:06 +0800

    change about link

--- source/modules/settings/about.js
+++ source/modules/settings/about.js
@@ -20,9 +20,7 @@ class About {
         <h2>${LANG['header']}<span> v${antSword['package']['version']}</span></h2>
         <p>
           <a href="https://github.com/AntSwordProject/AntSword"><i class="fa fa-github-alt"></i> GitHub</a> /
-          <a href="http://uyu.us"><i class="fa fa-home"></i> ${LANG['homepage']}</a> /
-          <a href="http://doc.u0u.us"><i class="fa fa-book"></i> ${LANG['document']}</a> /
-          <a href="http://shang.qq.com/wpa/qunwpa?idkey=51997458a52d534454fd15e901648bf1f2ed799fde954822a595d6794eadc521"><i class="fa fa-qq"></i> ${LANG['qqgroup']}</a>
+          <a href="http://doc.u0u.us"><i class="fa fa-book"></i> ${LANG['document']}</a>
         </p>
       </div>
     `);
