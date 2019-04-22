2e23e9ed3e52befdf006ff2ff9afe466be39b3cc
Medicean <Medici.Yan@Gmail.com>
2019-04-22 11:24:06 +0800

    (Doc:Decoder) 添加函数注释

--- source/core/php/decoder/base64.js
+++ source/core/php/decoder/base64.js
@@ -1,20 +1,32 @@
 /**
  * php::base64解码器
- * ? 利用php的base64_decode进行解码处理
  */
 
 'use strict';
 
 module.exports = {
-  asoutput: (tag_s, tag_e) => {
+  /**
+   * @returns {string} asenc 将返回数据base64编码
+   */
+  asoutput: () => {
     return `function asenc($out){
-      return base64_encode($out);
+      return @base64_encode($out);
     }
     `.replace(/\n\s+/g, '');
   },
+  /**
+   * 解码字符串
+   * @param {string} data 要被解码的字符串
+   * @returns {string} 解码后的字符串
+   */
   decode_str: (data) => {
     return Buffer.from(data, 'base64').toString();
   },
+  /**
+   * 解码 Buffer
+   * @param {string} data 要被解码的 Buffer
+   * @returns {string} 解码后的 Buffer
+   */
   decode_buff: (data) => {
     return Buffer.from(data.toString(), 'base64');
   }
